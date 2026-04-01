6ef10356447d50135cb4f4aff28952902263fcdb
unknown <2612082003@ACADEMICO.IESB.BR>
2026-04-01 09:37:01 -0300

    feat: conclui pratica01. Fecha #1

---
+++ praticas/pratica01/ola_mundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("ola,Mundo!");
+    return 0;
+    
+}
---
+++ praticas/pratica01/pratica01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main () { 
+   printf("ola pessoal\n");
+    return 0;
+}
